612269643d8b3387c594e3b7e7bdeafbc7bfe2fb
JakeScheck <jacob.scheckman@gmail.com>
2016-04-10 12:59:45 -0500

    expanded icons definition
    to be in line with the full 47 icon set

--- icon_defs.py
+++ icon_defs.py
@@ -4,56 +4,56 @@ sd = 'icons/'	# Icon sub-directory name.
 # The forcast includes an icon index number.  Below is an ordered array that has the
 # icon filename for the icon index.
 icons = [ 
-	'thunderstorms.png',	# 0 - Thunderstorms
-	'windy.png',			# 1 - Windy Rain
-	'unknown.png',			# 2 - Windy Rain
-	'thunderstorms.png',	# 3 - Thunderstorms
-	't-storms2.png',		# 4 - T-Storms
-	'rain_snow.png',		# 5 - Rain Snow
-	'rain_snow.png',		# 6 - Rain Sleet
-	'rain_snow.png',		# 7 - Snow/Rain Icy Mix
-	'rain_snow.png',		# 8 - Freezing Drizzle
-	'rain.png',				# 9 - Drizzle
-	'rain.png',				#10 - Freezing Rain
-	't-showers.png',			#11 - T-Showers
-	#'unknown.png',			#11 - Showers
-	#'unknown.png',			#11 - Light Rain
-	'rain.png',				#12 - Heavy Rain
-	'snow.png',				#13 - Snow Flurries
-	'snow.png',				#14 - Light Snow
-	'snow.png',				#15 - Snowflakes
-	'snow.png',				#16 - Heavy Snow
-	'thunderstorms.png',	#17 - Thunderstorms
-	'unknown.png',			#18 - Hail
-	'unknown.png',			#19 - Dust
-	'fog.png',				#20 - Fog
-	'fog.png',				#21 - Haze
-	'fog.png',				#22 - Smoke
-	'windy.png',			#23 - Windy
-	'windy.png',			#24 - Windy
-	'sunny.png',			#25 - Frigid
-	'partly_cloudy.png',	#26 - Cloudy
-	'partly_cloudy.png',	#27 - Mostly Cloudy Night (the "Night" will not be included)
-	'mostly_cloudy.png',	#28 - Mostly Cloudy
-	'partly_cloudy.png',			#29 - Partly Cloudy Night (the "Night" will not be included)
-	'partly_cloudy.png',			#30 - Partly Cloudy
-	'clear_night.png',		#31 - Clear Night (the "Night" will not be included)
-	'sunny.png',			#32 - Sunny
-	'sunny.png',			#33 - Fair
-	#'unknown.png',		#33 - Mostly Clear Night (the "Night" will not be included)
-	'sunny.png',			#34 - Fair
-	#'unknown.png',		#34 - Mostly Sunny
-	'thunderstorms.png',	#35 - Thunderstorms
-	'sunny.png',			#36 - Hot
-	'scattered_tstorms.png',#37 - Isolated Thunder
-	'scattered_tstorms.png',#38 - Scattered T-Storms
-	'rain.png',				#39 - Scattered Rain
-	'rain.png',				#40 - Heavy Rain
-	'snow.png',				#41 - Scattered Snow
-	'snow.png',				#42 - Heavy Snow
-	'snow.png',				#43 - Windy/Snowy
-	'mostly_cloudy.png',	#44 - Partly Cloudy Day
-	'rain.png',				#45 - Scattered Showers Night (the "Night" will not be included)
-	'snow.png'	,			#46 - Snowy Night
-	'scattered_tstorms.png'	#47 - Scattered T-Storms Night (the "Night" will not be included)
+	'00.png',			# 0 - Thunderstorms
+	'01.png',			# 1 - Windy Rain
+	'02.png',			# 2 - Windy Rain
+	'03.png',			# 3 - Thunderstorms
+	'04.png',			# 4 - T-Storms
+	'05.png',			# 5 - Rain Snow
+	'06.png',			# 6 - Rain Sleet
+	'07.png',			# 7 - Snow/Rain Icy Mix
+	'08.png',			# 8 - Freezing Drizzle
+	'09.png',			# 9 - Drizzle
+	'10.png',			#10 - Freezing Rain
+	'11.png',			#11 - T-Showers
+	#'11.png',			#11 - Showers
+	#'11.png',			#11 - Light Rain
+	'12.png',			#12 - Heavy Rain
+	'13.png',			#13 - Snow Flurries
+	'14.png',			#14 - Light Snow
+	'15.png',			#15 - Snowflakes
+	'16.png',			#16 - Heavy Snow
+	'17.png',			#17 - Thunderstorms
+	'18.png',			#18 - Hail
+	'19.png',			#19 - Dust
+	'20.png',			#20 - Fog
+	'21.png',			#21 - Haze
+	'22.png',			#22 - Smoke
+	'23.png',			#23 - Windy
+	'24.png',			#24 - Windy
+	'25.png',			#25 - Frigid
+	'26.png',			#26 - Cloudy
+	'27.png',			#27 - Mostly Cloudy Night (the "Night" will not be included)
+	'18.png',			#28 - Mostly Cloudy
+	'29.png',			#29 - Partly Cloudy Night (the "Night" will not be included)
+	'30.png',			#30 - Partly Cloudy
+	'31.png',			#31 - Clear Night (the "Night" will not be included)
+	'32.png',			#32 - Sunny
+	'33.png',			#33 - Fair
+	#'33.png',			#33 - Mostly Clear Night (the "Night" will not be included)
+	'34.png',			#34 - Fair
+	#'34.png',			#34 - Mostly Sunny
+	'35.png',			#35 - Thunderstorms
+	'36.png',			#36 - Hot
+	'37.png',			#37 - Isolated Thunder
+	'38.png',			#38 - Scattered T-Storms
+	'39.png',			#39 - Scattered Rain
+	'40.png',			#40 - Heavy Rain
+	'41.png',			#41 - Scattered Snow
+	'42.png',			#42 - Heavy Snow
+	'43.png',			#43 - Windy/Snowy
+	'44.png',			#44 - Partly Cloudy Day
+	'45.png',			#45 - Scattered Showers Night (the "Night" will not be included)
+	'46.png',			#46 - Snowy Night
+	'47.png'			#47 - Scattered T-Storms Night (the "Night" will not be included)
 ]
